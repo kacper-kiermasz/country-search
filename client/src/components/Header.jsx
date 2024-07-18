99714837267d7baabbbb68d822b125fef79ec591
Kacper Kiermasz <kacper.kiermasz@mhra.gov.uk>
2024-07-18 15:15:41 +0100

    add header alt text

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,6 +7,7 @@ function Header() {
       <img
         style={{ width: "100px" }}
         src={process.env.PUBLIC_URL + "world-image.jpg"}
+        alt="illustration of the earth from space"
       ></img>
       <p>This React application lets you search through the countries below</p>
     </div>
